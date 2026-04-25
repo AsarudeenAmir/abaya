362256894ca969ef5dab3bf9de90c5470c6d6504
AsarudeenAmir <108387997+AsarudeenAmir@users.noreply.github.com>
2026-04-25 15:55:30 +0530

    docker file

--- src/main/java/com/raqaf/abaya/AbayaApplication.java
+++ src/main/java/com/raqaf/abaya/AbayaApplication.java
@@ -1,10 +1,13 @@
 package com.raqaf.abaya;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AbayaApplication {
@@ -15,6 +18,17 @@ public class AbayaApplication {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*");
+            }
+        };
     }
 
 }
